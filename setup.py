ec087b033b8e9b4172a4d5e77a26a10ea16ffb64
Daniel Luque <danielluque14@gmail.com>
2012-05-29 20:04:10 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+
+__author__ = 'Daniel Luque <danielluque14@gmail.com>'
+__version__ = '1.0.0'
+
+setup(
+    name='pybooru',
+    version=__version__,
+    author=__author__ 
+    author_email="danielluque14@gmail.com",  
+    url="http://mundogeek.net/tutorial-python/",  
+    license="GPL",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['simplejson'],
+)
