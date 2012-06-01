208f78b7ad0033b1f04160f9a2252d2beb161eee
Daniel Luque <danielluque14@gmail.com>
2012-06-01 21:48:02 +0200

    Version number changed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 __author__ = 'Daniel Luque <danielluque14@gmail.com>'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 setup(
     name='pybooru',
