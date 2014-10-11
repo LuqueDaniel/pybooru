2fccd99cf55d041c8505033161e544b716ac664b
Daniel Luque <danielluque14@gmail.com>
2014-10-11 13:46:57 +0200

    Travis CI: Testing

---
+++ provisional_test.py
@@ -0,0 +1,7 @@
+# encoding: utf-8
+from pybooru import Pybooru
+
+#client = Pybooru(site_url='konachan.com')
+client = Pybooru(site_url="http://www.konachan.com")
+
+tags = client.tags_list(None, None, 100, 0, 'date')
