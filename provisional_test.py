a62f4c4e4c92d695faac4fb69293903e475cb60b
Daniel Luque <danielluque14@gmail.com>
2016-06-20 23:55:51 +0200

    Updated provisional_test.py

--- provisional_test.py
+++ provisional_test.py
@@ -1,7 +1,11 @@
 # encoding: utf-8
+from __future__ import print_function
 from pybooru import Pybooru
 
 # client = Pybooru(site_url='konachan.com')
 client = Pybooru(site_url="http://www.konachan.com")
 
 tags = client.tag_list(order='date')
+print(client.last_call)
+posts = client.post_list()
+print(client.last_call)
