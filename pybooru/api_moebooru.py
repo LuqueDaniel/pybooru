c446b94fd875881a850d263001e610a97a2bba4b
Daniel Luque <danielluque14@gmail.com>
2016-09-01 18:05:58 +0200

    Refactoring api_moebooru.py

--- pybooru/api_moebooru.py
+++ pybooru/api_moebooru.py
@@ -23,7 +23,7 @@ class MoebooruApi(object):
     """
 
     def post_list(self, **params):
-        """Get a list of posts.
+        """_get a list of posts.
 
         Parameters:
             tags: The tags to search for. Any tag combination that works on the
@@ -32,7 +32,7 @@ class MoebooruApi(object):
                    posts per request.
             page: The page number.
         """
-        return self._request('post', params)
+        return self._get('post', params)
 
     def post_create(self, tags, file_=None, rating=None, source=None,
                     rating_locked=None, note_locked=None, parent_id=None,
@@ -68,7 +68,7 @@ class MoebooruApi(object):
                 'post[parent_id]': parent_id,
                 'md5': md5}
             file_ = {'post[file]': open(file_, 'rb')}
-            return self._request('post/create', params, 'POST', file_)
+            return self._get('post/create', params, 'POST', file_)
         else:
             raise PybooruAPIError("'file_' or 'source' is required.")
 
@@ -103,9 +103,9 @@ class MoebooruApi(object):
             }
         if file_ is not None:
             file_ = {'post[file]': open(file_, 'rb')}
-            return self._request('post/update', params, 'PUT', file_)
+            return self._get('post/update', params, 'PUT', file_)
         else:
-            return self._request('post/update', params, 'PUT')
+            return self._get('post/update', params, 'PUT')
 
     def post_destroy(self, id_):
         """Function to destroy a specific post.
@@ -116,7 +116,7 @@ class MoebooruApi(object):
         Parameters:
             id_: The id number of the post to delete.
         """
-        return self._request('post/destroy', {'id': id_}, 'DELETE')
+        return self._get('post/destroy', {'id': id_}, 'DELETE')
 
     def post_revert_tags(self, id_, history_id):
         """Function to reverts a post to a previous set of tags
@@ -127,7 +127,7 @@ class MoebooruApi(object):
             history_id: The id number of the tag history.
         """
         params = {'id': id_, 'history_id': history_id}
-        return self._request('post/revert_tags', params, 'PUT')
+        return self._get('post/revert_tags', params, 'PUT')
 
     def post_vote(self, id_, score):
         """Action lets you vote for a post (Requires login).
@@ -142,12 +142,12 @@ class MoebooruApi(object):
         """
         if score <= 3:
             params = {'id': id_, 'score': score}
-            return self._request('post/vote', params, 'POST')
+            return self._get('post/vote', params, 'POST')
         else:
             raise PybooruAPIError("Value of 'score' only can be 0, 1, 2 or 3.")
 
     def tag_list(self, **params):
-        """Get a list of tags.
+        """_get a list of tags.
 
         Parameters:
             name: The exact name of the tag.
@@ -158,7 +158,7 @@ class MoebooruApi(object):
             order: Can be 'date', 'name' or 'count'.
             after_id: Return all tags that have an id number greater than this.
         """
-        return self._request('tag', params)
+        return self._get('tag', params)
 
     def tag_update(self, name=None, tag_type=None, is_ambiguous=None):
         """Action to lets you update tag (Requires login) (UNTESTED).
@@ -178,27 +178,27 @@ class MoebooruApi(object):
             'tag[tag_type]': tag_type,
             'tag[is_ambiguous]': is_ambiguous
             }
-        return self._request('tag/update', params, 'PUT')
+        return self._get('tag/update', params, 'PUT')
 
     def tag_related(self, **params):
-        """Get a list of related tags.
+        """_get a list of related tags.
 
         Parameters:
             tags: The tag names to query.
             type: Restrict results to this tag type. Can be general, artist,
                   copyright, or character.
         """
-        return self._request('tag/related', params)
+        return self._get('tag/related', params)
 
     def artist_list(self, **params):
-        """Get a list of artists.
+        """_get a list of artists.
 
         Parameters:
             name: The name (or a fragment of the name) of the artist.
             order: Can be date or name.
             page: The page number.
         """
-        return self._request('artist', params)
+        return self._get('artist', params)
 
     def artist_create(self, name, urls=None, alias=None, group=None):
         """Function to create a artist (Requires login) (UNTESTED).
@@ -218,7 +218,7 @@ class MoebooruApi(object):
             'artist[alias]': alias,
             'artist[group]': group
             }
-        return self._request('artist/create', params, 'POST')
+        return self._get('artist/create', params, 'POST')
 
     def artist_update(self, id_, name=None, urls=None, alias=None, group=None):
         """Function to update an artists.
@@ -243,7 +243,7 @@ class MoebooruApi(object):
             'artist[alias]': alias,
             'artist[group]': group
             }
-        return self._request('artist/update', params, 'PUT')
+        return self._get('artist/update', params, 'PUT')
 
     def artist_destroy(self, id_):
         """Action to lets you remove artist (Requires login) (UNTESTED).
@@ -251,15 +251,15 @@ class MoebooruApi(object):
         Parameters:
             id_: The id of the artist to destroy.
         """
-        return self._request('artist/destroy', {'id': id_}, 'POST')
+        return self._get('artist/destroy', {'id': id_}, 'POST')
 
     def comment_show(self, id_):
-        """Get a specific comment.
+        """_get a specific comment.
 
         Parameters:
             id_: The id number of the comment to retrieve.
         """
-        return self._request('comment/show', {'id': id_})
+        return self._get('comment/show', {'id': id_})
 
     def comment_create(self, post_id, comment_body, anonymous=None):
         """Action to lets you create a comment (Requires login).
@@ -275,7 +275,7 @@ class MoebooruApi(object):
                 'comment[body]': comment_body,
                 'comment[anonymous]': anonymous
                 }
-            return self._request('comment/create', params, 'POST')
+            return self._get('comment/create', params, 'POST')
         else:
             raise PybooruAPIError("Required 'post_id' and 'comment_body' "
                                   "parameters")
@@ -286,7 +286,7 @@ class MoebooruApi(object):
         Parameters:
             id_: The id number of the comment to remove.
         """
-        return self._request('comment/destroy', {'id': id_}, 'DELETE')
+        return self._get('comment/destroy', {'id': id_}, 'DELETE')
 
     def wiki_list(self, **params):
         """Function to retrieves a list of every wiki page.
@@ -297,7 +297,7 @@ class MoebooruApi(object):
             limit: The number of pages to retrieve (Default: 100).
             page: The page number.
         """
-        return self._request('wiki', params)
+        return self._get('wiki', params)
 
     def wiki_create(self, title, body):
         """Action to lets you create a wiki page (Requires login) (UNTESTED).
@@ -307,7 +307,7 @@ class MoebooruApi(object):
             body: The body of the wiki page.
         """
         params = {'wiki_page[title]': title, 'wiki_page[body]': body}
-        return self._request('wiki/create', params, 'POST')
+        return self._get('wiki/create', params, 'POST')
 
     def wiki_update(self, page_title, new_title=None, page_body=None):
         """Action to lets you update a wiki page (Requires login) (UNTESTED).
@@ -322,16 +322,16 @@ class MoebooruApi(object):
             'wiki_page[title]': new_title,
             'wiki_page[body]': page_body
             }
-        return self._request('wiki/update', params, 'PUT')
+        return self._get('wiki/update', params, 'PUT')
 
     def wiki_show(self, **params):
-        """Get a specific wiki page.
+        """_get a specific wiki page.
 
         Parameters:
             title: The title of the wiki page to retrieve.
             version: The version of the page to retrieve.
         """
-        return self._request('wiki/show', params)
+        return self._get('wiki/show', params)
 
     def wiki_destroy(self, title):
         """Function to delete a specific wiki page (Requires login)
@@ -340,7 +340,7 @@ class MoebooruApi(object):
         Parameters:
             title: The title of the page to delete.
         """
-        return self._request('wiki/destroy', {'title': title}, 'DELETE')
+        return self._get('wiki/destroy', {'title': title}, 'DELETE')
 
     def wiki_lock(self, title):
         """Function to lock a specific wiki page (Requires login)
@@ -349,7 +349,7 @@ class MoebooruApi(object):
         Parameters:
             title: The title of the page to lock.
         """
-        return self._request('wiki/lock', {'title': title}, 'POST')
+        return self._get('wiki/lock', {'title': title}, 'POST')
 
     def wiki_unlock(self, title):
         """Function to unlock a specific wiki page (Requires login)
@@ -358,7 +358,7 @@ class MoebooruApi(object):
         Parameters:
             title: The title of the page to unlock.
         """
-        return self._request('wiki/unlock', {'title': title}, 'POST')
+        return self._get('wiki/unlock', {'title': title}, 'POST')
 
     def wiki_revert(self, title, version):
         """Function to revert a specific wiki page (Requires login) (UNTESTED).
@@ -368,23 +368,23 @@ class MoebooruApi(object):
             version: The version to revert to.
         """
         params = {'title': title, 'version': version}
-        return self._request('wiki/revert', params, 'PUT')
+        return self._get('wiki/revert', params, 'PUT')
 
     def wiki_history(self, title):
-        """Get history of specific wiki page.
+        """_get history of specific wiki page.
 
         Parameters:
             title: The title of the wiki page to retrieve versions for.
         """
-        return self._request('wiki/history', {'title': title})
+        return self._get('wiki/history', {'title': title})
 
     def note_list(self, **params):
-        """Get note list.
+        """_get note list.
 
         Parameters:
             post_id: The post id number to retrieve notes for.
         """
-        return self._request('note', params)
+        return self._get('note', params)
 
     def note_search(self, query):
         """Search specific note.
@@ -392,10 +392,10 @@ class MoebooruApi(object):
         Parameters:
             query: A word or phrase to search for.
         """
-        return self._request('note/search', {'query': query})
+        return self._get('note/search', {'query': query})
 
     def notes_history(self, **params):
-        """Get history of notes.
+        """_get history of notes.
 
         Parameters:
             post_id: The post id number to retrieve note versions for.
@@ -403,7 +403,7 @@ class MoebooruApi(object):
             limit: How many versions to retrieve (Default: 10).
             page: The note id number to retrieve versions for.
         """
-        return self._request('note/history', params)
+        return self._get('note/history', params)
 
     def note_revert(self, id_, version):
         """Function to revert a specific note (Requires login) (UNTESTED).
@@ -413,7 +413,7 @@ class MoebooruApi(object):
             version: The version to revert to.
         """
         params = {'id': id_, 'version': version}
-        return self._request('note/revert', params, 'PUT')
+        return self._get('note/revert', params, 'PUT')
 
     def note_create_update(self, post_id=None, coor_x=None, coor_y=None,
                            width=None, height=None, is_active=None, body=None,
@@ -442,51 +442,51 @@ class MoebooruApi(object):
             'note[body]': body,
             'note[is_active]': is_active
             }
-        return self._request('note/update', params, 'POST')
+        return self._get('note/update', params, 'POST')
 
     def user_search(self, **params):
         """Search users.
 
-        If you don't specify any parameters you'll get a listing of all users.
+        If you don't specify any parameters you'll _get a listing of all users.
 
         Parameters:
             id: The id number of the user.
             name: The name of the user.
         """
-        return self._request('user', params)
+        return self._get('user', params)
 
     def forum_list(self, **params):
-        """Function to get forum posts.
+        """Function to _get forum posts.
 
-        If you don't specify any parameters you'll get a listing of all users.
+        If you don't specify any parameters you'll _get a listing of all users.
 
         Parameters:
             parent_id: The parent ID number. You'll return all the responses
                        to that forum post.
         """
-        return self._request('forum', params)
+        return self._get('forum', params)
 
     def pool_list(self, **params):
-        """Function to get pools.
+        """Function to _get pools.
 
-        If you don't specify any parameters you'll get a list of all pools.
+        If you don't specify any parameters you'll _get a list of all pools.
 
         Parameters:
             query: The title.
             page: The page.
         """
-        return self._request('pool', params)
+        return self._get('pool', params)
 
     def pool_posts(self, **params):
-        """Function to get pools posts.
+        """Function to _get pools posts.
 
-        If you don't specify any parameters you'll get a list of all pools.
+        If you don't specify any parameters you'll _get a list of all pools.
 
         Parameters:
             id: The pool id number.
             page: The page.
         """
-        return self._request('pool/show', params)
+        return self._get('pool/show', params)
 
     def pool_update(self, id_, name=None, is_public=None,
                     description=None):
@@ -504,7 +504,7 @@ class MoebooruApi(object):
             'pool[is_public]': is_public,
             'pool[description]': description
             }
-        return self._request('pool/update', params, 'PUT')
+        return self._get('pool/update', params, 'PUT')
 
     def pool_create(self, name, description, is_public):
         """Function to create a pool (Require login) (UNTESTED).
@@ -516,7 +516,7 @@ class MoebooruApi(object):
         """
         params = {'pool[name]': name, 'pool[description]': description,
                   'pool[is_public]': is_public}
-        return self._request('pool/create', params, 'POST')
+        return self._get('pool/create', params, 'POST')
 
     def pool_destroy(self, id_):
         """Function to destroy a specific pool (Require login) (UNTESTED).
@@ -524,7 +524,7 @@ class MoebooruApi(object):
         Parameters:
             id_: The pool id number.
         """
-        return self._request('pool/destroy', {'id': id_}, 'DELETE')
+        return self._get('pool/destroy', {'id': id_}, 'DELETE')
 
     def pool_add_post(self, **params):
         """Function to add a post (Require login) (UNTESTED).
@@ -533,7 +533,7 @@ class MoebooruApi(object):
             pool_id: The pool to add the post to.
             post_id: The post to add.
         """
-        return self._request('pool/add_post', params, 'PUT')
+        return self._get('pool/add_post', params, 'PUT')
 
     def pool_remove_post(self, **params):
         """Function to remove a post (Require login) (UNTESTED).
@@ -542,7 +542,7 @@ class MoebooruApi(object):
             pool_id: The pool to remove the post to.
             post_id: The post to remove.
         """
-        return self._request('pool/remove_post', params, 'PUT')
+        return self._get('pool/remove_post', params, 'PUT')
 
     def favorite_list_users(self, id_):
         """Function to return a list with all users who have added to favorites
@@ -551,6 +551,6 @@ class MoebooruApi(object):
         Parameters:
             id_: The post id.
         """
-        response = self._request('favorite/list_users', {'id': id_})
+        response = self._get('favorite/list_users', {'id': id_})
         # Return list with users
         return response['favorited_users'].split(',')
