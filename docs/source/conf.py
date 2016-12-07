ca88aea3aef1bd96957dcbac4133cde4f287a4b0
Daniel Luque <danielluque14@gmail.com>
2016-12-07 16:41:08 +0100

    Docs: change html_theme
    Docs: change html_theme to use sphinx_rtd_theme for Read the Docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,7 +84,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# Use Read the Docs theme
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -153,6 +155,3 @@ texinfo_documents = [
      author, 'Pybooru', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
